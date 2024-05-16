9e451ef9202b8648e5fcc4fc0399d53ff2056bf9
Truphy <truphenaokenye@gmail.com>
2024-05-16 19:59:56 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import './App.css';
+import Props from './Rendering';
+import My_name from './States'
+function App() {
+
+  return (
+    <>
+    <div>
+      <My_name/>
+    </div>
+    </>
+  )
+}
+
+export default App
